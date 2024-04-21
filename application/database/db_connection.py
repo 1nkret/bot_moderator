445aab4565e0bc1360afca8cb3848d92fac0c330
1nkret <dima.dls.ua@gmail.com>
2024-04-21 10:31:37 +0300

    updated database

---
+++ application/database/db_connection.py
@@ -0,0 +1,15 @@
+from sqlite3 import connect
+
+
+class DbConnection:
+    """
+    Class to connect to db
+    """
+    def __init__(self):
+        self.connection = connect('database.db')
+
+    def __enter__(self):
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
